5b6f7dd9562645fb1257f3a41f9ffa4c26a5f7e5
Jon Redeker <jon.redeker@gmail.com>
2018-05-02 14:09:48 -0400

    add functional tests for initial user open and retrieve

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 # http://www.obeythetestinggoat.com/book/chapter_02_unittest.html
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -17,16 +19,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # the page loads and sees the title
         self.assertIn('Remember', self.browser.title)
-        self.fail('test not complete')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Remember', header_text)
 
         # user is invited to enter a first remember item
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter an item to remember'
+        )
 
         # user enters a first remember item of "swimsuit"
+        input_box.send_keys('swimsuit')
 
         # when the user hits enter, the page updates, and the page now lists:
         # "1: buy swimsuit for vacation"
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: swimsuit' for row in rows))
 
         # there is still a text boy for more items.  user enters a second item "sun tan lotion"
+        self.fail('working, but test not finished')
 
         # the page updates again, and now shows both items on the list
 
