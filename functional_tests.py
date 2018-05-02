1771757fe30bfd09d0da83031dba29dd95da1319
Jon Redeker <jon.redeker@gmail.com>
2018-05-02 16:54:44 -0400

    fixed bug in functional test

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: milk' for row in rows),
             "New Grocery List item did not appear in table"
