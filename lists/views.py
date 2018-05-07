0a81cc324e294e0e6c5beccd4f1ada6cf2b68726
Jon Redeker <jon.redeker@gmail.com>
2018-05-07 17:18:44 -0400

    update view for loop through items

--- lists/views.py
+++ lists/views.py
@@ -10,5 +10,7 @@ def home_page(request):
             Item.objects.create(text=request.POST['item_text'])
             return redirect('/')
 
-    return render(request, 'home.html')
+    items = Item.objects.all()
+
+    return render(request, 'home.html', {'items': items})
 
