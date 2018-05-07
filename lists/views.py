37c8a351c35e7aaaf3dde5580fa5568fe8e49a7a
Jon Redeker <jon.redeker@gmail.com>
2018-05-07 15:38:35 -0400

    update template to display multiple list items

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 
@@ -6,10 +6,9 @@ from lists.models import Item
 def home_page(request):
 
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
+        if request.method == 'POST':
+            Item.objects.create(text=request.POST['item_text'])
+            return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-    })
+    return render(request, 'home.html')
 
