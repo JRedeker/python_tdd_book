8388340f50e96983e7df66531c3f27c80ed34ada
Jon Redeker <jon.redeker@gmail.com>
2018-05-07 13:17:52 -0400

    working on updating view with new list

--- lists/tests.py
+++ lists/tests.py
@@ -21,10 +21,19 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_post_request(self):
-        response = self.client.post('/', data={'item_text': 'A new grocery item'})
-        self.assertIn('A new grocery item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+    def test_can_save_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
@@ -45,4 +54,5 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'first list item')
         self.assertEqual(second_saved_item.text, 'second item')
+
 
