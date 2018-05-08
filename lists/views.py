1ffd06a08daf2f78c2fcd2b3afa48fc20530598b
Jon Redeker <jon.redeker@gmail.com>
2018-05-08 11:38:50 -0400

    urls for new forms, moved code in views, refactor tests, remove uneeded tests

--- lists/views.py
+++ lists/views.py
@@ -4,16 +4,13 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-
-    if request.method == 'POST':
-        if request.method == 'POST':
-            Item.objects.create(text=request.POST['item_text'])
-            return redirect('/lists/the-list')
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    return redirect('/lists/the-list')
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,5 @@ from lists import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home'),
-    path('lists/the-list', views.view_list, name='view_list')
+    path('lists/the-list/$', views.view_list, name='view_list')
 ]
