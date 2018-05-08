be4f7587091b19785359b70fe6f7b762bccf32a4
Jon Redeker <jon.redeker@gmail.com>
2018-05-08 12:59:00 -0400

    new migration after fixing foreignkey in table

--- lists/models.py
+++ lists/models.py
@@ -8,4 +8,4 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None)
+    list = models.ForeignKey(List, on_delete=models.PROTECT, default=None)
